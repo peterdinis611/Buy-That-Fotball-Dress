965b27b304b4d35a3994d61cf4a6aca0ba684c35
peterdinis <peterdinis611@gmail.com>
2026-08-28 15:17:49 +0200

    infra: Adding rabittmq

---
+++ src/Contracts/AuctionUpdated.cs
@@ -0,0 +1,25 @@
+namespace Contracts;
+
+public class AuctionUpdated
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; }
+    public string Seller { get; set; } = string.Empty;
+    public string? Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime AuctionEnd { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public string Club { get; set; } = string.Empty;
+    public string PlayerName { get; set; } = string.Empty;
+    public int? PlayerNumber { get; set; }
+    public string Season { get; set; } = string.Empty;
+    public string Size { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
+    public string KitType { get; set; } = string.Empty;
+    public string Condition { get; set; } = string.Empty;
+    public string? League { get; set; }
+    public string? ImageUrl { get; set; }
+}
