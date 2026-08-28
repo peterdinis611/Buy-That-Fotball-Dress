bcfd68431a0ae2761565014dee044377950c62d8
peterdinis <peterdinis611@gmail.com>
2026-08-28 15:28:38 +0200

    feat: GatewayService

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("frontend", policy =>
+    {
+        var origins = builder.Configuration.GetSection("ClientApps").Get<string[]>()
+            ?? ["http://localhost:3000"];
+
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(origins);
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("frontend");
+app.MapReverseProxy();
+
+app.Run();
