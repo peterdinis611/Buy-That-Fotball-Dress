50726a5f0271860cccb052fee2147b200bd9ae45
peterdinis <peterdinis611@gmail.com>
2026-08-30 12:28:08 +0200

    feat: Adding rabittmq to notificationService

--- src/NotificationService/obj/Debug/net10.0/NotificationService.AssemblyInfo.cs
+++ src/NotificationService/obj/Debug/net10.0/NotificationService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124754943f8bf77639025b81fa70ae5e089ac959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0138bf45072e41dfd246bad415abc7eca536a3fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
