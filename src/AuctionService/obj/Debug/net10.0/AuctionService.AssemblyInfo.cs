442b4117b252fd83d83618360d398f5bd7e6bb5c
peterdinis <peterdinis611@gmail.com>
2026-09-01 17:27:07 +0200

    feat: Branch merge

--- src/AuctionService/obj/Debug/net10.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net10.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ce0ee8bcfd520f6f58767cb429245ccecce964")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836d09d82a423779b478b360952a4baea4750c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
