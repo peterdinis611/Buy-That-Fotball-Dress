3f1b84fbae6afc7a8eea9d96277016ea45fa0e8e
peterdinis <peterdinis611@gmail.com>
2026-09-01 19:14:53 +0200

    fix: Fixing packages issues

--- src/Contracts/obj/Debug/net10.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net10.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63db50b3dc26bf514ed4220575d19084a297d1c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2f52a44d6b350b1fe4eeac3d9391edbb856217")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
